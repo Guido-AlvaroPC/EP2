6ff57ec7e8be7be5c0f8a15171163601b24cfc39
Guido-AlvaroPC <92442134+Guido-AlvaroPC@users.noreply.github.com>
2021-10-28 22:36:22 -0300

    Update README.py

--- README.py
+++ README.py
@@ -6,8 +6,6 @@ def criando_as_pecas():
     lista_pecas=[(6,6),(6,5),(6,4),(6,3),(6,2),(6,1),(6,0),(5,5),(5,4),(5,3),(5,2),(5,1),(5,0),(4,4),(4,3),(4,2),(4,1),(4,0),(3,3),(3,2),(3,1),(3,0),(2,2),(2,1),(2,0),(1,1),(1,0),(0,0)]
     rd.shuffle(lista_pecas)
     return lista_pecas
-print(criando_as_pecas())
-print(len(criando_as_pecas()))
 
 def destribui_pecas(lista_pecas,num_participantes):
     dicionario_mesa={}
